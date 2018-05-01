a65a91d2758b480027115aec545a934759061920
Damian Juzyk <damian.juzyk@gmail.com>
2018-05-02 01:22:46 +0200

    merge

--- js/arch.js
+++ js/arch.js
@@ -1,3 +1,29 @@
+// zadanie 12.4
+
+
+var url = 'http://api.icndb.com/jokes/random';
+
+var button = document.getElementById("get-joke");
+button.addEventListener("click", function() {
+  getJoke();
+});
+
+var paragraph = document.getElementById("joke");
+
+function getJoke() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.addEventListener("load", function() {
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+};
+
+paragraph.innerHTML = response.value.joke;
+
+// zadanie 12.5
+
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
@@ -17,6 +43,8 @@ function createTweet(input) {
         quoteAuthor = "Unknown author";
     }
 
+
+
   var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
   if (tweetText.length > 140) {
@@ -30,8 +58,8 @@ function createTweet(input) {
 }
 
 $(document).ready(function() {
-    getQuote();                             // cytat pojawia sie od razu
+    getQuote();
     $(".trigger").click(function() {
-        getQuote();                         // cytat po kliknieciu w przycisk
+        getQuote();
     })
 });
